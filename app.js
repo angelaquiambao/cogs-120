4341526e69bb941df330571e97ebc5550fc50e11
angelaquiambao <38932147+angelaquiambao@users.noreply.github.com>
2020-03-05 16:00:40 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -7,10 +7,40 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://cogs120bash.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("users");
+
+var usersRef = ref.child("preferences");
+usersRef.set({
+    test1: {
+        genres: ["Action"],
+        friends: ["A. Dude","B. Nice"]
+    }
+});
 
 var index = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
+var suggestions = require('./routes/suggestions');
+var add = require('./routes/add');
+var friends = require('./routes/friends');
+var chats = require('./routes/chats');
+var chatrooms = require('./routes/chatrooms');
+var login = require('./routes/login');
+var games = require('./routes/games');
+var profile = require('./routes/profile');
+var editprofile = require('./routes/editprofile');
+var suggB = require('./routes/suggestions_b');
+var suggA = require('./routes/suggestions_a');
+var newchat = require('./routes/newchat');
 
 var app = express();
 
@@ -34,9 +64,21 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', index.view);
+app.get('/home', index.view);
 // Example route
 // app.get('/users', user.list);
+app.get('/suggestions', suggestions.view);
+app.get('/friends', friends.view);
+app.get('/chats', chats.view);
+app.get('/chatrooms', chatrooms.view);
+app.get('/', login.view);
+app.get('/games', games.view);
+app.get('/add', add.view);
+app.get('/profile', profile.view);
+app.get('/editprofile', editprofile.view);
+app.get('/suggestions_b', suggB.view);
+app.get('/suggestions_a', suggA.view);
+app.get('/newchat', newchat.view);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
